ac82be963ac04e8ef4cb987aca6e9f46520f23cb
SauravKumarLal <sauravkrlal@gmail.com>
2024-07-31 16:01:35 +0530

    Test DONE

--- api/app.js
+++ api/app.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import authRoute from "./routes/auth.route.js"
 import postRoute from "./routes/post.route.js"
+import testRoute from "./routes/test.route.js";
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ const app = express();
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
+app.use("/api/test", testRoute);
 
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
