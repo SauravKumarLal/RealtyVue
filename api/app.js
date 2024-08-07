be334b31fa8405b6063c385a42aba2fdc4ad8fb7
SauravKumarLal <sauravkrlal@gmail.com>
2024-08-07 19:42:48 +0530

    profile upload done

--- api/app.js
+++ api/app.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import authRoute from "./routes/auth.route.js"
 import postRoute from "./routes/post.route.js"
 import testRoute from "./routes/test.route.js";
+import userRoute from "./routes/user.route.js";
 
 dotenv.config();
 
@@ -13,10 +14,11 @@ const app = express();
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.use("/api/test", testRoute);
 
 app.use("/api/posts", postRoute);
+app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/test", testRoute);
 
 const PORT = process.env.PORT || 8800;
 
