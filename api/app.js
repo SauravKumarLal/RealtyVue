65dc0e013156426d3f67786a790e4ff17c71dc63
SauravKumarLal <sauravkrlal@gmail.com>
2024-08-11 15:11:45 +0530

    chat integration backend done

--- api/app.js
+++ api/app.js
@@ -6,6 +6,8 @@ import authRoute from "./routes/auth.route.js"
 import postRoute from "./routes/post.route.js"
 import testRoute from "./routes/test.route.js";
 import userRoute from "./routes/user.route.js";
+import chatRoute from "./routes/chat.route.js";
+import messageRoute from "./routes/message.route.js";
 
 dotenv.config();
 
@@ -19,6 +21,8 @@ app.use("/api/posts", postRoute);
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/test", testRoute);
+app.use("/api/chats", chatRoute);
+app.use("/api/messages", messageRoute);
 
 const PORT = process.env.PORT || 8800;
 
